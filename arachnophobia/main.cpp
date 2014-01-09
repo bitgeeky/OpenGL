dbcd6c97fba02815c0fc395c38950d9d7013614d
bitgeeky <mozpankaj1994@gmail.com>
2014-01-09 19:12:05 +0530

    opening a window with a box

---
+++ arachnophobia/main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <cmath>
+#include <GL/glut.h>
+using namespace std;
+
+#define PI 3.141592653589
+#define DEG2RAD(deg) (deg * PI / 180)
+
+// Function Declarations
+void drawScene();
+void update(int value);
+void drawBox(float len);
+void initRendering();
+void handleResize(int w, int h);
+void handleKeypress1(unsigned char key, int x, int y);
+void handleKeypress2(int key, int x, int y);
+void handleMouseclick(int button, int state, int x, int y);
+
+// Global Variables
+float box_len = 4.0f;
+
+
+int main(int argc, char **argv) {
+
+    // Initialize GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+    int w = glutGet(GLUT_SCREEN_WIDTH);
+    int h = glutGet(GLUT_SCREEN_HEIGHT);
+    int windowWidth = w * 2 / 3;
+    int windowHeight = h * 2 / 3;
+
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutInitWindowPosition((w - windowWidth) / 2, (h - windowHeight) / 2);
+
+    glutCreateWindow("Arachnophobia");  // Setup the window
+    initRendering();
+
+    // Register callbacks
+    glutDisplayFunc(drawScene);
+    glutIdleFunc(drawScene);
+    glutKeyboardFunc(handleKeypress1);
+    glutSpecialFunc(handleKeypress2);
+    glutMouseFunc(handleMouseclick);
+    glutReshapeFunc(handleResize);
+    glutTimerFunc(10, update, 0);
+
+    glutMainLoop();
+    return 0;
+}
+
+// Function to draw objects on the screen
+void drawScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();
+
+    // Draw Box
+    glTranslatef(0.0f, 0.0f, -5.0f);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    drawBox(box_len);
+    
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+// Function to handle all calculations in the scene
+// updated evry 10 milliseconds
+void update(int value) {
+    
+    glutTimerFunc(10, update, 0);
+}
+
+void drawBox(float len) {
+   
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glBegin(GL_QUADS);
+    glVertex2f(-len / 2, -len / 2);
+    glVertex2f(len / 2, -len / 2);
+    glVertex2f(len / 2, len / 2);
+    glVertex2f(-len / 2, len / 2);
+    glEnd();
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+// Initializing some openGL 3D rendering options
+void initRendering() {
+
+    glEnable(GL_DEPTH_TEST);        // Enable objects to be drawn ahead/behind one another
+    glEnable(GL_COLOR_MATERIAL);    // Enable coloring
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);   // Setting a background color
+}
+
+// Function called when the window is resized
+void handleResize(int w, int h) {
+
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (float)w / (float)h, 0.1f, 200.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void handleKeypress1(unsigned char key, int x, int y) {
+
+    if (key == 27) {
+        exit(0);     // escape key is pressed
+    }
+}
+
+void handleKeypress2(int key, int x, int y) {
+
+}
+
+void handleMouseclick(int button, int state, int x, int y) {
+
+}
